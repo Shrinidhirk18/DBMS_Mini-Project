8853946bba020b47c9505cb428ccac7e406cc80a
Shrinidhirk18 <70753612+Shrinidhirk18@users.noreply.github.com>
2020-12-22 14:14:03 +0530

    Add files via upload

---
+++ first.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  text-align: center;
+  padding-top: 140px;
+  font-size: 50px;
+  background-image:url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToEFjej7771vco4D8ZqLZZ7K-WFfyrTE5TXw&usqp=CAU');
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+}
+.button {
+  background-color: black;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 30px;
+  margin: 4px 2px;
+  cursor: pointer;
+}
+
+a{
+text-decoration:none;
+color: white;
+}
+
+h1{
+  color: rebeccapurple;
+  font-size: 80px;
+}
+
+
+</style>
+</head>
+<body>
+
+<h1>Movie Ticket Booking</h1>
+
+
+<button class="button"><a href="login.php">Log in</a></button>
+
+
+</body>
+</html>
